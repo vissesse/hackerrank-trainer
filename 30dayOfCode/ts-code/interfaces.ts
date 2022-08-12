47b68bcefe782cf2569b9a63c08d0a319d141ed9
Carlos Vissesse <carloscarlycsony@gmail.com>
2022-08-12 23:52:49 +0100

    interface cases solved usind ts

---
+++ 30dayOfCode/ts-code/interfaces.ts
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function (inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function (): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+
+function main() {
+    // Enter your code here
+    interface AdvancedArithmetic {
+
+        divisorSum(n: number): number;
+    }
+
+    class Calculator implements AdvancedArithmetic {
+        divisorSum(n: number): number {
+            let sumDiv: number = 0;
+            for (let i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    sumDiv += i
+                }
+            }
+            return sumDiv;
+        }
+    }
+    let value = parseInt(readLine());
+    let calculator = new Calculator();
+    let divorSum = calculator.divisorSum(value)
+    console.log(`I implemented: AdvancedArithmetic`);
+    console.log(divorSum);
+
+
+}
